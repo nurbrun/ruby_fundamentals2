e4875bf5ee504ffdd19cc454d703beb255ed5631
Bruno Alfano-Rizzo <bruno.alfano.rizzo@gmail.com>
2014-10-22 20:50:47 +0000

    sorted the list alphabetically

--- exercise6.rb
+++ exercise6.rb
@@ -10,8 +10,6 @@ puts "the number of groceries on my grocery list are #{grocery_list.count}"
 
 banana_output = grocery_list.include?("banana")
 
-puts banana_output
-
 if banana_output==true
 	puts "you don't need to pick up bananas"
 elsif banana_output==false
@@ -22,3 +20,9 @@ end
 
 
 puts "the second item on the list is #{grocery_list[1]}"
+
+puts "Oh no the list isn't sorted! Let's try this again"
+
+grocery_list.sort.each do |item|
+	puts "*"+item
+end
