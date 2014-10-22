7914beab08a737c080b91ea8335b5cf63a3e1ed8
Bruno Alfano-Rizzo <bruno.alfano.rizzo@gmail.com>
2014-10-22 20:44:44 +0000

    do we need bananas?

--- exercise6.rb
+++ exercise6.rb
@@ -8,3 +8,17 @@ end
 
 puts "the number of groceries on my grocery list are #{grocery_list.count}"
 
+banana_output = grocery_list.include?("banana")
+
+puts banana_output
+
+if banana_output==true
+	puts "you don't need to pick up bananas"
+elsif banana_output==false
+	puts "you need bananas, monkey!"
+else
+	puts "bananas don't seem to exist in this universe"
+end
+
+
+
