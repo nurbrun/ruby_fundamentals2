03b61f59ee45dd399ce4dec5eec8d78a3d8f80df
Bruno Alfano-Rizzo <bruno.alfano.rizzo@gmail.com>
2014-10-22 22:19:20 +0000

    added 5% to each class

--- exercise7.rb
+++ exercise7.rb
@@ -4,14 +4,18 @@ students = {
 	:cohort3 => 22
 }
 
+students [:cohort4]=42
+
 def list_cohorts (data)
 
-	data.each {|key, value| puts "#{key}: #{value} students"}
+	data.each {|key, value| puts "#{key}: #{value*105/100} students"}
 
 end	
 
 list_cohorts(students)
 
-students [:cohort4]=42
+
 
 puts students.keys
+
+
